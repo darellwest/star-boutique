f36f2bb581fbdbf026b12fd2fb673a20066d8870
darellwest <ewere.njoagwu@gmail.com>
2021-10-07 04:50:07 +0100

    added header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
-    <switch>
-      <Route exact  path='/' component={HomePage} />
-      <Route exact  path='/shop' component={ShopPage} />
-    </switch>
+    <div>
+      <Header />
+      <switch>
+        <Route exact  path='/' component={HomePage} />
+        <Route exact  path='/shop' component={ShopPage} />
+      </switch>
+    </div>
   );
 }
 
